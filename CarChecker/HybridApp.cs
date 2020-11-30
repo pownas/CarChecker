ab497322dbc99b466c4ce819b01f8135f4648284
Jan-Willem Spuij <jws@birdseyeview.nl>
2020-11-30 14:24:58 +0100

    Make the application run.
    - We make sure that the application builds again and that it runs inside
      the browser.

--- CarChecker/HybridApp.cs
+++ CarChecker/HybridApp.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 using Microsoft.MobileBlazorBindings;
-using Xamarin.Essentials;
 using Xamarin.Forms;
+using System.Net.Http;
+using Microsoft.MobileBlazorBindings.Authentication;
 
 namespace CarChecker
 {
     public class HybridApp : Application
     {
+        private const string BaseAddress = "https://localhost:5001/";
+
         public HybridApp(IFileProvider fileProvider = null)
         {
             var hostBuilder = MobileBlazorBindingsHost.CreateDefaultBuilder()
@@ -19,6 +21,30 @@ namespace CarChecker
                     services.AddBlazorHybrid();
 
                     // Register app-specific services
+                    // Configure HttpClient for use when talking to server backend
+                    services.AddHttpClient("CarChecker.ServerAPI",
+                        client => client.BaseAddress = new Uri(BaseAddress))
+                        .AddHttpMessageHandler(() => new ApiAuthorizationMessageHandler(BaseAddress));
+
+                    // Add protected storage for storage of refresh tokens
+                    services.AddProtectedStorage();
+
+                    // Other DI services
+                    //                    services.AddScoped<ILocalVehiclesStore, LocalVehiclesStore>();
+
+                    // Add the http client as the default to inject.
+                    services.AddScoped<HttpClient>(sp =>
+                    {
+                        var accessTokenProvider = sp.GetRequiredService<IAccessTokenProvider>();
+                        var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+                        ApiAuthorizationMessageHandler.RegisterTokenProvider(BaseAddress, accessTokenProvider);
+                        return httpClientFactory.CreateClient("CarChecker.ServerAPI");
+                    });
+
+                    services.AddApiAuthorization(BaseAddress);
+//                    services.AddScoped<AccountClaimsPrincipalFactory<RemoteUserAccount>, OfflineAccountClaimsPrincipalFactory>();
+                    services.AddLocalization(options => options.ResourcesPath = "Resources");
+
                 })
                 .UseWebRoot("wwwroot");
 
